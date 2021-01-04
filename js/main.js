cd80f3fb4ff87da4b15acc2064c3a57c9b22a389
senasan08 <76098080+senasan08@users.noreply.github.com>
2021-01-04 17:14:22 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+$(function(){
+    $('#typo') .on('click',function(){
+        $('#typo,inner').animate({
+            color: "#ebc000",
+            fontSize:'80px'
+        },
+        1500
+        );
+      });
+});
